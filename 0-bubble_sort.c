8e8d9476e90adefe509e79aeb551e42d6adbdd6a
Jany Muong <me.roumuong@gmail.com>
2023-04-25 19:12:08 +0300

    Bubble sort; its Big-O

---
+++ 0-bubble_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+/**
+ * swap - swaps elements based on a comparison operator
+ *
+ * @xp: pointer to base element
+ * @yp: pointer to comparison element(to swap with)
+ *
+ * return: void
+ */
+void swap(int *xp, int *yp)
+{
+	int tmp = *xp;
+	*xp = *yp;
+	*yp = tmp;
+}
+
+/**
+ * bubble_sort - iteratively steps throug elements,
+ * compares the current element with the one after it,
+ * swapping values.
+ *
+ * @array: (arg) array of integers
+ * @size:  size of the array, determines inner loop
+ *
+ * return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+
+	if (size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+		}
+	}
+}
