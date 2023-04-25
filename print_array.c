1df9d6dbe3eefc55fb1f7bf2da56206a51ebc6ac
Jany Muong <me.roumuong@gmail.com>
2023-04-25 18:05:56 +0300

    print array; print list

---
+++ print_array.c
@@ -0,0 +1,25 @@
+#include "sort.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_array - prints an array of integers
+ *
+ * @array: the array to be printed
+ * @size: count of (int)elements in @array
+ */
+void print_array(const int *array, size_t size)
+{
+	size_t i;
+
+	i = 0;
+	while (array && i < size)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", array[i]);
+		++i;
+	}
+
+	printf("\n");
+}
