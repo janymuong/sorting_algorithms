61f2171ed93a67b8863442579b779f76d4c5eed6
Jany Muong <me.roumuong@gmail.com>
2023-04-25 19:26:06 +0300

    bubble sort: optimize

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -28,19 +28,26 @@ void swap(int *xp, int *yp)
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
+	int swapped;
 
 	if (size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
+		swapped = 0;
+
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				swap(&array[j], &array[j + 1]);
 				print_array(array, size);
+				swapped = 1;
 			}
 		}
+
+		if (swapped == 0)
+			break;
 	}
 }
