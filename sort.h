82e9b5fdaabcd4074389c03f4d371eb3ddcb10ad
Jany Muong <me.roumuong@gmail.com>
2023-04-25 18:44:49 +0300

    header file

--- sort.h
+++ sort.h
@@ -21,6 +21,11 @@ typedef struct listint_s
 /* prototpe function for sawpping */
 void swap(int *xp, int *yp);
 
+/* prototypes for: printing elements */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+/* prototypes for: algorithms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
